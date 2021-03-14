69a3294d8860483827546d0795dbba0be465a76e
Isao Jonas <isao.jonas@gmail.com>
2021-03-14 13:56:18 -0500

    simplify http

--- http/go.mod
+++ http/go.mod
@@ -5,6 +5,4 @@ go 1.15
 require (
 	github.com/jonasi/gr8go/log v0.0.1
 	github.com/jonasi/gr8go/service v0.0.1
-	github.com/julienschmidt/httprouter v1.3.0
-	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822
 )
--- http/log.go
+++ http/log.go
@@ -23,36 +23,3 @@ func logServerListeningCleanupErr(ctx context.Context, addr net.Addr, err error)
 		"error": err,
 	})
 }
-
-func logRouterHandlingRoute(ctx context.Context, method string, path string, mws []string) {
-	if method == "*" {
-		gr8log.Info(ctx, "Handling all methods for route", gr8log.Args{
-			"path":        path,
-			"middlewares": mws,
-		})
-	} else {
-		gr8log.Info(ctx, "Handling route", gr8log.Args{
-			"method":      method,
-			"path":        path,
-			"middlewares": mws,
-		})
-	}
-}
-
-func logRouterHandlingNotFound(ctx context.Context, middlewares []string) {
-	gr8log.Info(ctx, "Handling not found", gr8log.Args{
-		"middlewares": middlewares,
-	})
-}
-
-func logTemplateHandlerError(ctx context.Context, err error) {
-	gr8log.Error(ctx, "Template render error", gr8log.Args{
-		"error": err,
-	})
-}
-
-func logSPAIndexHandlerError(ctx context.Context, err error) {
-	gr8log.Error(ctx, "Error making index handler", gr8log.Args{
-		"error": err,
-	})
-}
